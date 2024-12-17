f8c9b07c8d24d2e548c5866fc3188fc61fb2f072
Paulo Santos <paulo@vontage.com.br>
2024-12-17 16:43:24 -0300

    WIP

--- packages/app/functions/api/helloworld.ts
+++ packages/app/functions/api/helloworld.ts
@@ -3,5 +3,5 @@ import type { PagesFunction } from '@cloudflare/workers-types'
 interface Env {}
 
 export const onRequest: PagesFunction<Env> = async (_context) => {
-  return Response.json(({ time: `time is ${new Date().toISOString()}` }))
+  return Response.json(({ time: `the time is ${new Date().toISOString()}` }))
 }
