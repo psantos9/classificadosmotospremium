b9844625cf189a36844605d97ae52ae617a503cc
Paulo Santos <paulo@vontage.com.br>
2024-12-21 20:22:20 -0300

    WIP

--- packages/app/src/helpers/printAppVersionConsoleBanner.ts
+++ packages/app/src/helpers/printAppVersionConsoleBanner.ts
@@ -1,3 +1,3 @@
 export const printAppVersionConsoleBanner = () => {
-  console.log(`Vontage Player ${__APP_RELEASE__} v${__APP_VERSION__} ${import.meta.env.MODE}`)
+  console.log(`Classificados Motos Premium ${__APP_RELEASE__} v${__APP_VERSION__} ${import.meta.env.MODE}`)
 }
