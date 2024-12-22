556d6a433976069765ae8a2bfb8a2f0ff3079b1b
Paulo Santos <paulo@vontage.com.br>
2024-12-21 22:13:39 -0300

    WIP

--- packages/app/_functions/api/v1/healthcheck.ts
+++
@@ -1,6 +0,0 @@
-import type { ContextData, Env } from '@/types'
-import type { PagesFunction } from '@cloudflare/workers-types'
-
-export const onRequestGet: PagesFunction<Env, any, ContextData> = async (_context) => {
-  return Response.json({ ok: true })
-}
