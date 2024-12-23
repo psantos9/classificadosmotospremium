c0a3b5e9cebaf9725039121bbedf571352c9c62f
Paulo Santos <paulo@vontage.com.br>
2024-12-23 17:32:10 -0300

    WIP

--- packages/app/src/components/ValidateEmailForm.vue
+++ packages/app/src/components/ValidateEmailForm.vue
@@ -5,7 +5,7 @@
       ref="emailEl"
       v-model="email"
       v-bind="emailAttrs"
-      class="block w-full rounded-md bg-white py-1.5 pl-10 pr-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[var(--primary)] sm:pl-9 sm:text-sm/6"
+      class="block w-full rounded-md bg-white py-1.5 pl-10 pr-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[var(--primary)] sm:pl-9 sm:text-sm/6 lowercase"
       placeholder="E-mail"
       :invalid="!!errors.email"
     >
@@ -53,10 +53,10 @@ const validateEmail = async () => {
     return
   }
   loading.value = true
-
+  const _email = unref(email)?.toLowerCase() as string
   try {
-    const exists = await api.validateEmail(unref(email) as string)
-    emit('emailVerified', { email: unref(email) as string, exists })
+    const exists = await api.validateEmail(_email)
+    emit('emailVerified', { email: _email, exists })
   }
   finally {
     loading.value = false
