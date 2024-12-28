9e4c18c4474edacbcd96e09731314455ba7b2184
Paulo Santos <paulo@vontage.com.br>
2024-12-28 08:58:39 -0300

    WIP

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="test h-screen">
-    <div class="flex flex-col">
-      <Header class="sticky top-0 z-50" />
-      <Sidebar />
-      <div class="overflow-y-hidden">
-        <RouterView />
-      </div>
+  <div class="test h-screen flex flex-col">
+    <Header class="sticky top-0 z-50" />
+    <Sidebar />
+    <div class="flex-1 overflow-y-auto flex flex-col">
+      <RouterView />
     </div>
   </div>
 </template>
