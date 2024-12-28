89f5bfbd77abddc07ee132dd9e1da224a4f7e34b
Paulo Santos <paulo@vontage.com.br>
2024-12-28 14:41:47 -0300

    WIP

--- packages/app/src/components/VehicleCard.vue
+++ packages/app/src/components/VehicleCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-white shadow rounded-md h-auto flex flex-col overflow-hidden group cursor-pointer hover:shadow-lg">
+  <div class="bg-white shadow rounded-md h-auto flex flex-col overflow-hidden group cursor-pointer">
     <div class="overflow-hidden">
-      <img :src="api.getImageUrl(anuncio.fotos[0])" class="h-full w-full aspect-video object-cover group-hover:scale-110 transition-all">
+      <img :src="api.getImageUrl(anuncio.fotos[0])" class="h-full w-full aspect-video object-cover group-hover:scale-110 transition-all duration-200">
     </div>
 
     <div class="flex flex-col p-4 text-sm gap-1">
