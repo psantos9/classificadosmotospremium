56f397c68af4a6b5fbaa78d58cb07f58e24f1545
Paulo Santos <paulo@vontage.com.br>
2025-01-03 22:03:27 -0300

    WIP

--- packages/app/src/views/Anuncio.vue
+++ packages/app/src/views/Anuncio.vue
@@ -139,7 +139,7 @@
       </div>
     </div>
 
-    <div class="rounded-md shadow border bg-gray-100 p-4 flex flex-col gap-4 self-start">
+    <div class="rounded-md shadow border bg-gray-100 p-4 flex flex-col gap-4 self-start w-full">
       <div class="text-lg font-black">
         Sobre o anunciante
       </div>
