8b7c1fe03a74ad8841948ea38b4298a7d42c664d
Paulo Santos <paulo@vontage.com.br>
2025-01-04 08:21:23 -0300

    WIP

--- packages/api/src/routes/api/v1/images.ts
+++ packages/api/src/routes/api/v1/images.ts
@@ -8,7 +8,7 @@ export const router = AutoRouter<IAppAuthenticatedRequest, [Env, ExecutionContex
 })
   .get('/:b64ImageKey', async (req, env, ctx) => {
     const cache = caches.default
-    let res = await cache.match(req)
+    let res = await cache.match(req.clone())
     if (res) {
       return res
     }
@@ -32,6 +32,35 @@ export const router = AutoRouter<IAppAuthenticatedRequest, [Env, ExecutionContex
       headers.set('Content-Type', contentType)
     }
     res = new Response(object.body, { headers })
-    ctx.waitUntil(cache.put(req, res.clone()))
+    ctx.waitUntil(cache.put(req.clone(), res.clone()))
+    return res
+  })
+  .get('/:b64ImageKey/thumbnail', async (req, env, ctx) => {
+    const cache = caches.default
+    let res = await cache.match(req.clone())
+    if (res) {
+      return res
+    }
+    const imageKey = atob(req.params.b64ImageKey)
+    const object = await env.AD_IMAGES_BUCKET.get(imageKey)
+    if (object === null) {
+      return error(400, 'image not found')
+    }
+    const contentType = object.customMetadata?.type ?? null
+    const headers = new Headers()
+    object.writeHttpMetadata(headers)
+    headers.set('etag', object.httpEtag)
+    headers.set('size', object.size.toString())
+    headers.set('Access-Control-Expose-Headers', 'etag')
+    headers.set('access-control-allow-origin', '*')
+    // add caching header, configured here for 1-year
+    headers.set('cache-control', `public, max-age=31536000`)
+    // vary header so cache respects content-negotiation/auto-format
+    headers.set('vary', 'Accept')
+    if (contentType !== null) {
+      headers.set('Content-Type', contentType)
+    }
+    res = new Response(object.body, { headers })
+    ctx.waitUntil(cache.put(req.clone(), res.clone()))
     return res
   })
