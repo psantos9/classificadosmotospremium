b87471d3c371c50798a47782ab3d6607bf8cb430
Paulo Santos <paulo@vontage.com.br>
2025-01-03 22:06:38 -0300

    WIP

--- packages/app/src/components/SearchComponent.vue
+++ packages/app/src/components/SearchComponent.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="relative z-10 flex flex-col items-center justify-center md:rounded-xl bg-black p-12 md:mx-14">
-    <div class="uppercase font-bold text-2xl tracking-wide  text-white">
+  <div class="relative z-10 flex flex-col items-center justify-center md:rounded-xl bg-black p-4 md:p-12 md:mx-14">
+    <div class="uppercase font-bold text-base md:text-2xl tracking-wide  text-white">
       Encontre o veículo ideal para você
     </div>
-    <div class="mt-8 flex items-center gap-8 w-full max-w-3xl">
-      <Autocomplete :data="['teste', 'ok']" class="flex-1" />
+    <div class="mt-4 flex flex-col items-center gap-4 md:gap-8 w-full max-w-3xl">
+      <Autocomplete :data="['teste', 'ok']" class="flex-1 w-full" />
       <button
         class="bg-[var(--primary)] hover:bg-[var(--primary-lighter)] rounded-md h-[3.5rem] uppercase text-sm font-black flex gap-2 items-center justify-center px-8 transition-colors"
         @click="$router.push({ name: 'anuncios' })"
@@ -14,7 +14,7 @@
       </button>
     </div>
     <div>
-      <button class="mt-6 border border-[var(--primary)] hover:border-[var(--primary-lighter)] text-[var(--primary)] hover:text-[var(--primary-lighter)] rounded-md text-base font-bold flex gap-2 items-center justify-center px-4 py-1 transition-colors">
+      <button class="mt-4 md:mt-6 border border-[var(--primary)] hover:border-[var(--primary-lighter)] text-[var(--primary)] hover:text-[var(--primary-lighter)] rounded-md text-base font-bold flex gap-2 items-center justify-center px-4 py-1 transition-colors">
         <FontAwesomeIcon :icon="faSliders" />
         <span>Busca avançada</span>
       </button>
