c5db82193f80083400b691eef9236ce285b1cf55
Paulo Santos <paulo@vontage.com.br>
2025-01-06 11:01:56 -0300

    WIP

---
+++ packages/app/src/components/HighlightsComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="uppercase text-xl md:text-3xl py-8">
+      <span class="font-light">
+        Anúncios em
+      </span>
+      <strong>destaque</strong>
+    </div>
+    <div class="flex flex-wrap gap-4 overflow-y-auto items-start w-full container mx-auto">
+      <template v-if="loading">
+        <div v-for="i in [...Array(10).keys()]" :key="i" class="bg-gray-200 w-full h-full rounded-md border animate-pulse min-h-[335px]" />
+      </template>
+      <template v-else>
+        <VehicleCard
+          v-for="anuncio in anuncios"
+          :key="anuncio.id"
+          :anuncio="anuncio"
+          class="md:w-[250px]"
+          @click="$router.push({ name: 'anuncio', params: { id: anuncio.id } })"
+        />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { PublicAd } from '@cmp/shared/models/database/models'
+import VehicleCard from '@/components/VehicleCard.vue'
+import { useApp } from '@/composables/useApp'
+import { faFilter } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { ref } from 'vue'
+
+const { api } = useApp()
+const loading = ref(false)
+const anuncios = ref<PublicAd[]>([])
+const fetchAds = async () => {
+  try {
+    loading.value = true
+    anuncios.value = await api.fetchAnuncios()
+  }
+  finally {
+    loading.value = false
+  }
+}
+
+fetchAds()
+</script>
--- packages/app/src/views/HomeView.vue
+++ packages/app/src/views/HomeView.vue
@@ -2,10 +2,12 @@
   <div class="flex flex-col flex-1 bg-gray-100">
     <HomeSwiper />
     <SearchComponent class="md:translate-y-[-30px]" />
+    <HighlightsComponent class="md:translate-y-[-30px]" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import HighlightsComponent from '@/components/HighlightsComponent.vue'
 import HomeSwiper from '@/components/HomeSwiper.vue'
 import SearchComponent from '@/components/SearchComponent.vue'
 </script>
