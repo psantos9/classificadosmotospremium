8ed3c9d7b9ff6f2d18c0a82af0ac46cda8f18e63
Paulo Santos <paulo@vontage.com.br>
2025-01-06 15:12:15 -0300

    WIP

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -18,7 +18,7 @@
       v-if="y > 0"
       type="button"
       class="fixed bottom-0 right-0 w-16 h-16 md:w-10 md:h-10 m-2 shadow-md bg-[var(--primary)] hover:bg-[var(--primary-darker)] text-[var(--primary-text)] focus:outline-none rounded-full text-center flex justify-center items-center"
-      @click="scrollToTop"
+      @click.stop="scrollToTop"
     >
       <FontAwesomeIcon :icon="faArrowUp" fixed-width size="2x" class="md:hidden" />
       <FontAwesomeIcon :icon="faArrowUp" fixed-width size="xl" class="hidden md:block" />
