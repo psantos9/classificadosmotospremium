c37871eb6618cff7d55ca5838d120f7503ffcdbb
Paulo Santos <paulo@vontage.com.br>
2025-01-06 19:12:39 -0300

    WIP

--- packages/app/src/components/Header.vue
+++ packages/app/src/components/Header.vue
@@ -1,41 +1,43 @@
 <template>
-  <header class="main-header">
-    <div v-if="signature" class="hidden md:block absolute bottom-0 right-1 text-xs text-gray-500">
-      {{ signature }}
-    </div>
-    <div class="flex items-center">
-      <SidebarButton class="block md:hidden" />
-      <button class="w-36 md:w-52 md:ml-4" @click.stop="navigateToHome">
-        <img src="@/assets/images/full_logo_dark.svg">
-      </button>
-    </div>
-
-    <div class="flex items-center gap-6 justify-end pr-4">
-      <div class="hidden md:flex md:items-center gap-2">
-        <RouterLink
-          v-for="(item, i) in menuItems" :key="i" v-slot="{ isExactActive, navigate }" :to="item.to" custom
-        >
-          <span
-            class="px-2 py-1 text-sm font-medium cursor-pointer uppercase transition-color"
-            :class="[
-              isExactActive ? 'text-[var(--primary)]' : 'hover:text-[var(--primary-lighter)]',
-            ]"
-            @click="navigate"
-          >
-            {{ item.label }}
-          </span>
-        </RouterLink>
+  <div class="bg-[var(--main-header-bg)]">
+    <header class="main-header">
+      <div v-if="signature" class="hidden md:block absolute bottom-0 right-1 text-xs text-gray-500">
+        {{ signature }}
       </div>
-      <button
-        class="bg-[var(--primary)] hover:bg-[var(--primary-lighter)] text-[var(--primary-text)] px-4 py-2 rounded-md text-sm md:text-base font-bold flex items-center gap-2"
-        @click="$router.push({ name: 'anuncie' })"
-      >
-        <span>Anuncie</span>
-        <FontAwesomeIcon :icon="faBullhorn" />
-      </button>
-      <SignInButton class="hidden md:block max-w-[150px] shrink-0" />
-    </div>
-  </header>
+      <div class="flex items-center">
+        <SidebarButton class="block md:hidden" />
+        <button class="w-36 md:w-52 md:ml-4" @click.stop="navigateToHome">
+          <img src="@/assets/images/full_logo_dark.svg">
+        </button>
+      </div>
+
+      <div class="flex items-center gap-6 justify-end pr-4">
+        <div class="hidden md:flex md:items-center gap-2">
+          <RouterLink
+            v-for="(item, i) in menuItems" :key="i" v-slot="{ isExactActive, navigate }" :to="item.to" custom
+          >
+            <span
+              class="px-2 py-1 text-sm font-medium cursor-pointer uppercase transition-color"
+              :class="[
+                isExactActive ? 'text-[var(--primary)]' : 'hover:text-[var(--primary-lighter)]',
+              ]"
+              @click="navigate"
+            >
+              {{ item.label }}
+            </span>
+          </RouterLink>
+        </div>
+        <button
+          class="bg-[var(--primary)] hover:bg-[var(--primary-lighter)] text-[var(--primary-text)] px-4 py-2 rounded-md text-sm md:text-base font-bold flex items-center gap-2"
+          @click="$router.push({ name: 'anuncie' })"
+        >
+          <span>Anuncie</span>
+          <FontAwesomeIcon :icon="faBullhorn" />
+        </button>
+        <SignInButton class="hidden md:block max-w-[150px] shrink-0" />
+      </div>
+    </header>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -59,5 +61,5 @@ const navigateToHome = async () => {
 <style lang="sass" scoped>
 .main-header
   @apply bg-[var(--main-header-bg)] text-white h-[var(--main-header-size)]
-  @apply flex items-center justify-between relative
+  @apply flex items-center justify-between relative md:container md:mx-auto
 </style>
