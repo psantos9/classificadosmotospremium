f238c2425b8c4ae8e979deb0efe30f2e341e458a
Paulo Santos <paulo@vontage.com.br>
2025-01-13 16:06:09 -0300

    WIP

--- packages/app/src/views/MeusAnuncios.vue
+++ packages/app/src/views/MeusAnuncios.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="view-container flex-1 flex flex-col">
     <div class="card flex-1 flex flex-col">
-      <div class="card-header cursor-pointer data-[loading=true]:pointer-events-none data-[loading=true]:text-[var(--primary)] transition-colors" :data-loading="loading" @click="!loading ? fetchAds() : undefined">
-        Meus Anúncios
+      <div class="flex items-center justify-between p-8" :data-loading="loading" @click="!loading ? fetchAds() : undefined">
+        <span class="text-xl font-black uppercase">
+          Meus Anúncios
+        </span>
+        <div>
+          <button
+            type="button"
+            class="w-full px-3 py-2 text-xs font-medium text-center inline-flex items-center gap-2 text-[var(--primary-text)] bg-[var(--primary)] rounded-md hover:bg-[var(--primary-darker)] focus:outline-none transition-colors"
+            @click="$router.push({ name: 'anuncie' })"
+          >
+            <FontAwesomeIcon :icon="faPlus" />
+            Criar Anúncio
+          </button>
+        </div>
       </div>
       <div class="flex-1 flex flex-column items-start gap-2 py-2 overflow-auto">
         <div
@@ -10,11 +22,11 @@
           :key="ad.id"
           class="bg-white w-full mx-2 px-4 py-2 rounded-md shadow flex gap-2"
         >
-          <div class="flex-1">
+          <div class="flex-1 flex flex-col gap-2">
             <div class="font-bold">
               {{ ad.marca }} {{ ad.modelo }} {{ ad.ano }}/{{ ad.anoModelo }}
             </div>
-            <div>{{ ad.status }} rev{{ ad.revision }}</div>
+            <!-- <div>{{ ad.status }} rev{{ ad.revision }}</div> -->
             <div class="flex flex-col text-sm gap-1">
               <div><span class="font-semibold">Valor:</span> {{ Intl.NumberFormat('pr-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(ad.preco) }}</div>
               <div><span class="font-semibold">Data criação:</span> {{ format(ad.createdAt, 'dd/MM/yyyy') }}</div>
@@ -57,7 +69,7 @@
 <script lang="ts" setup>
 import type { Anuncio } from '@cmp/shared/models/database/models'
 import { useApp } from '@/composables/useApp'
-import { faDeleteLeft, faEdit, faEye, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faDeleteLeft, faEdit, faEye, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { format } from 'date-fns'
 import { ref } from 'vue'
