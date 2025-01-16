11a587c0e8798e9f7364e2ffe3d35f9cfa456299
Paulo Santos <paulo@vontage.com.br>
2025-01-16 11:06:17 -0300

    WIP

--- packages/app/src/views/Conversa.vue
+++ packages/app/src/views/Conversa.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="view-container bg-gray-200 max-w-screen-md mx-auto p-4 flex-1 flex flex-col gap-4 overflow-hidden">
+  <div class="view-container bg-gray-200 max-w-screen-md mx-auto p-2 md:p-4 flex-1 flex flex-col gap-2 md:gap-4 overflow-hidden">
     <ThreadCard v-if="thread" :thread="thread" />
 
     <div ref="scrollContainer" class="flex-1 flex flex-col items-start gap-2 px-2 overflow-auto rounded-md">
