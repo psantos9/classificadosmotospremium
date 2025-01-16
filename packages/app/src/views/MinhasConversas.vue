4a3183da0a1f1510b4ca299eb762122f40e7f9ae
Paulo Santos <paulo@vontage.com.br>
2025-01-16 11:10:31 -0300

    WIP

--- packages/app/src/views/MinhasConversas.vue
+++ packages/app/src/views/MinhasConversas.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="view-container flex-1 flex flex-col gap-2 overflow-hidden">
-    <div class="flex items-center justify-between md:p-4 bg-[var(--primary-lightest)] md:rounded-md">
+    <div class="flex items-center justify-between p-2 md:p-4 bg-[var(--primary-lightest)] md:rounded-md">
       <span class="text-xl font-black uppercase">
         Minhas Conversas
       </span>
     </div>
-    <div class="flex-1 flex flex-col items-start gap-2 py-2 px-2 md:px-4 overflow-auto rounded-md">
+    <div class="flex-1 flex flex-col items-start gap-2 px-2 md:px-4 overflow-auto rounded-md">
       <ThreadCard
         v-for="thread in threads" :key="thread.id"
         :thread="thread"
