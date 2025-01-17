9c717a537d1e794d951b1cf9c31937562f7cf5c1
Paulo Santos <paulo@vontage.com.br>
2025-01-17 14:32:27 -0300

    WIP

--- packages/app/src/views/QuemSomos.vue
+++ packages/app/src/views/QuemSomos.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="view-container">
     <div class="card">
-      <div class="card-header">
+      <div class="p-4 md:p-8 flex items-center justify-center uppercase font-black text-2xl">
         Quem somos
       </div>
-      <div>quem somos</div>
+      <div class="flex flex-col items-center p-4 md:p-8">
+        <div class="max-w-screen-sm flex flex-col gap-4 md:gap-8 text-sm md:text-base">
+          <p>
+            O “Classificados Motos Premium” surgiu da necessidade de preencher uma lacuna no mercado de compra e vendas de motos no segmento de alto valor.
+          </p>
+          <p>
+            Nosso objetivo é unir, em uma única plataforma, VENDEDORES e COMPRADORES interessados em comercializar motos em um segmento onde a procura por qualidade, exclusividade e segurança são itens essenciais para um bom negócio.
+          </p>
+          <p>
+            Dentro desse segmento, anunciar em nossa plataforma permite que o vendedor atinja uma alta quantidade de compradores interessados exclusivamente nesse tipo de motocicleta em todo o Brasil, permitindo vendas locais e interestaduais de maneira simples e rápida.
+          </p>
+          <p>
+            Venha fazer parte você também, nossa comunidade cresce a cada dia, siga-nos no Instagram <a href="https://www.instagram.com/classificadosmotospremium" class="font-bold text-[var(--primary)] underline">@classificadosmotospremium</a>, fique por dentro de todas as novidades e receba em primeira mão os melhores anúncios de moto do Brasil.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
