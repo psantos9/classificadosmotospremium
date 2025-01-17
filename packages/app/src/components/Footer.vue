53025403d892b8ed8f93a5261b1a64306e480f3c
Paulo Santos <paulo@vontage.com.br>
2025-01-17 15:38:32 -0300

    WIP

--- packages/app/src/components/Footer.vue
+++ packages/app/src/components/Footer.vue
@@ -1,16 +1,14 @@
 <template>
   <footer class="bg-black text-white">
-    <div class="mx-auto max-w-7xl p-4 flex flex-col gap-4 md:gap-0 md:flex-row md:items-center md:justify-between lg:px-8">
-      <div class="flex justify-center gap-x-6 md:order-2">
-        <a v-for="item in navigation" :key="item.name" :href="item.href">
-          <span class="sr-only">{{ item.name }}</span>
-          <FontAwesomeIcon :icon="item.icon" size="2x" />
-        </a>
-      </div>
-      <p class="text-center text-sm/6 md:order-1 flex items-center justify-center gap-2">
+    <div class="mx-auto max-w-7xl p-4 flex items-center  justify-between">
+      <p class="text-center text-sm/6 flex items-center justify-center gap-2">
         <img src="@/assets/images/full_logo_dark.svg" class="w-[150px]">
         &copy; {{ new Date().getFullYear() }}
       </p>
+      <a v-for="item in navigation" :key="item.name" :href="item.href">
+        <span class="sr-only">{{ item.name }}</span>
+        <FontAwesomeIcon :icon="item.icon" size="2x" />
+      </a>
     </div>
   </footer>
 </template>
